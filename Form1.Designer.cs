cd79b892f6817175c71f7658c69f579482b2f03d
hinokyo <101516030+hinokyo@users.noreply.github.com>
2022-05-18 03:18:15 +0700

    Create Form1.Designer.cs

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+namespace lancuoiiiii
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.danhMucToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.chatLieuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hangHoaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hoaDonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.thoatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.danhMucToolStripMenuItem,
+            this.hoaDonToolStripMenuItem,
+            this.thoatToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // danhMucToolStripMenuItem
+            // 
+            this.danhMucToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.chatLieuToolStripMenuItem,
+            this.hangHoaToolStripMenuItem});
+            this.danhMucToolStripMenuItem.Name = "danhMucToolStripMenuItem";
+            this.danhMucToolStripMenuItem.Size = new System.Drawing.Size(90, 24);
+            this.danhMucToolStripMenuItem.Text = "Danh Muc";
+            // 
+            // chatLieuToolStripMenuItem
+            // 
+            this.chatLieuToolStripMenuItem.Name = "chatLieuToolStripMenuItem";
+            this.chatLieuToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.chatLieuToolStripMenuItem.Text = "Chat Lieu";
+            this.chatLieuToolStripMenuItem.Click += new System.EventHandler(this.chatLieuToolStripMenuItem_Click);
+            // 
+            // hangHoaToolStripMenuItem
+            // 
+            this.hangHoaToolStripMenuItem.Name = "hangHoaToolStripMenuItem";
+            this.hangHoaToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.hangHoaToolStripMenuItem.Text = "Hang Hoa";
+            // 
+            // hoaDonToolStripMenuItem
+            // 
+            this.hoaDonToolStripMenuItem.Name = "hoaDonToolStripMenuItem";
+            this.hoaDonToolStripMenuItem.Size = new System.Drawing.Size(83, 24);
+            this.hoaDonToolStripMenuItem.Text = "Hoa Don";
+            // 
+            // thoatToolStripMenuItem
+            // 
+            this.thoatToolStripMenuItem.Name = "thoatToolStripMenuItem";
+            this.thoatToolStripMenuItem.Size = new System.Drawing.Size(61, 24);
+            this.thoatToolStripMenuItem.Text = "Thoat";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem danhMucToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem chatLieuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hangHoaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hoaDonToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem thoatToolStripMenuItem;
+    }
+}
